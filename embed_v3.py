3c941814a868d9e1d5d7316433151d8d7dee1635
pranav.nair <pranav.nair@sculptsoft.com>
2025-03-11 14:47:50 +0530

    text similarity approach

--- embed_v3.py
+++ embed_v3.py
@@ -1,6 +1,7 @@
 import os
 import json
 import base64
+from typing import List
 
 from langchain_chroma import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -10,63 +11,173 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser
 
 llm = ChatOpenAI(
-    model='gpt-4o-mini',
+    model='gpt-4o',
     temperature=0.2,
     model_kwargs={"response_format": {"type": "json_object"}}
 )
 
-path = '/home/le-106/project/whiteboard/images_v2'
 
-image_uris = sorted(
-    [
-        os.path.join(path, image_name)
-        for image_name in os.listdir(path)
-        if image_name.endswith(".png")
-    ]
-)
+class SceneImageInformation(BaseModel):
+    """Scene information"""
+    visual_description: str = Field(description="visual description of the image")
+    characters: List[str] = Field(description="detailed keyword of characters inside the image")
+    props: List[str] = Field(description="detailed keyword of props inside the image")
+    motions: List[str] = Field(description="detailed keyword of animated motion inside the image")
 
-IMG_DESC = []
 
-class ImageInformation(BaseModel):
- """Image description"""
- description: str = Field(description="image description")
+class CharacterInformation(BaseModel):
+    """Character information"""
+    visual_description: str = Field(description="Precise description of the character")
 
-parser = JsonOutputParser(pydantic_object=ImageInformation)
+class PropInformation(BaseModel):
+    """Prop information"""
+    visual_description: str = Field(description="Precise description of the prop")
 
-def fetch_chain(inputs):
-    prompt = '''
-    Provide the full detailed description of the image 
-    '''
-    msg = llm.invoke(
+
+def embed_scenes():
+    path = '/home/le-106/project/whiteboard/images_v2/scenes/'
+
+    image_uris = sorted(
         [
-            SystemMessage(
-                content=[
-                    {"type": "text", "text": prompt},
-                    {"type": "text", "text": parser.get_format_instructions()},
-                ]
-            ),
-            HumanMessage(
-                content=[
-                    {"type": "image_url", "image_url": {"url": f"{inputs['image']}"}}
-                ]
-            )
+            os.path.join(path, image_name)
+            for image_name in os.listdir(path)
+            if image_name.endswith(".png")
         ]
     )
-    return msg.content
 
-for img_uri in image_uris:
-    with open(img_uri, "rb") as image_file:
-        base64_string = base64.b64encode(image_file.read()).decode("utf-8")
-    msg = fetch_chain(inputs={"image": f"data:image/png;base64,{base64_string}"})
-    ans_payload = json.loads(msg)
-    print(ans_payload['description'])
-    IMG_DESC.append(ans_payload['description'])
+    IMG_DESC = []
 
-img_vectorstore = Chroma(
-    collection_name="hospital_img_openai",
-    persist_directory='./chroma',
-    embedding_function=OpenAIEmbeddings()
-)
+    parser = JsonOutputParser(pydantic_object=SceneImageInformation)
 
-img_vectorstore.add_texts(texts=IMG_DESC, metadatas=[{"id": ind, "uri": uri} for ind, uri in enumerate(image_uris)])
-print("Text embeddings saved successfully")
+    def fetch_chain(inputs):
+        prompt = '''
+        Provide the full detailed description of the image. Also provide list of characters, props and animation motion visible in the image. 
+        '''
+        prompt = [
+                SystemMessage(
+                    content=[
+                        {"type": "text", "text": prompt},
+                        {"type": "text", "text": parser.get_format_instructions()},
+                    ]
+                ),
+                HumanMessage(
+                    content=[
+                        {"type": "image_url", "image_url": {"url": f"{inputs['image']}"}}
+                    ]
+                )
+            ]
+        msg = llm.invoke(
+            prompt
+        )
+        return msg.content
+
+    for ind, img_uri in enumerate(image_uris):
+        with open(img_uri, "rb") as image_file:
+            base64_string = base64.b64encode(image_file.read()).decode("utf-8")
+        msg = fetch_chain(inputs={"image": f"data:image/png;base64,{base64_string}"})
+        ans_payload = json.loads(msg)
+        ans_payload.update({"id": ind, "uri": img_uri})
+        ans_payload['characters'] = ",".join(ans_payload['characters'])
+        ans_payload['props'] = ",".join(ans_payload['props'])
+        ans_payload['motions'] = ",".join(ans_payload['motions'])
+        print("Scene->", ans_payload)
+        IMG_DESC.append(ans_payload)
+
+    img_vectorstore = Chroma(
+        collection_name="whiteboard_scenes_v1",
+        persist_directory='./chroma',
+        embedding_function=OpenAIEmbeddings()
+    )
+
+    img_vectorstore.add_texts(texts=[obj['visual_description'] for obj in IMG_DESC], metadatas=IMG_DESC)
+    print("Scene embeddings saved successfully")
+
+def embed_props_and_characters():
+    character_path = '/home/le-106/project/whiteboard/images_v2/characters/'
+    props_path = '/home/le-106/project/whiteboard/images_v2/props/'
+
+    character_image_uris = sorted(
+        [
+            os.path.join(character_path, image_name)
+            for image_name in os.listdir(character_path)
+            if image_name.endswith(".png")
+        ]
+    )
+    props_image_uris = sorted(
+        [
+            os.path.join(props_path, image_name)
+            for image_name in os.listdir(props_path)
+            if image_name.endswith(".png")
+        ]
+    )
+
+    IMG_DESC = []
+
+    def fetch_chain(inputs, type):
+        if type == "character":
+            parser = JsonOutputParser(pydantic_object=CharacterInformation)
+            prompt = '''
+            Provide the precise description of the character. 
+            '''
+        else:
+            parser = JsonOutputParser(pydantic_object=PropInformation)
+            prompt = '''
+            Provide the precise description of the prop. 
+            '''
+        prompt = [
+                SystemMessage(
+                    content=[
+                        {"type": "text", "text": prompt},
+                        {"type": "text", "text": parser.get_format_instructions()},
+                    ]
+                ),
+                HumanMessage(
+                    content=[
+                        {"type": "image_url", "image_url": {"url": f"{inputs['image']}"}}
+                    ]
+                )
+            ]
+        msg = llm.invoke(
+            prompt
+        )
+        return msg.content
+
+    for ind, img_uri in enumerate(character_image_uris):
+        with open(img_uri, "rb") as image_file:
+            base64_string = base64.b64encode(image_file.read()).decode("utf-8")
+        msg = fetch_chain(inputs={"image": f"data:image/png;base64,{base64_string}"}, type='characters')
+        ans_payload = json.loads(msg)
+        ans_payload.update({"id": ind, "uri": img_uri})
+        print("Character->", ans_payload)
+        IMG_DESC.append(ans_payload)
+
+    character_vectorstore = Chroma(
+        collection_name="whiteboard_characters_v1",
+        persist_directory='./chroma',
+        embedding_function=OpenAIEmbeddings()
+    )
+
+    character_vectorstore.add_texts(texts=[obj['visual_description'] for obj in IMG_DESC], metadatas=IMG_DESC)
+    print("Character embeddings saved successfully")
+
+    IMG_DESC = []
+    for ind, img_uri in enumerate(props_image_uris):
+        with open(img_uri, "rb") as image_file:
+            base64_string = base64.b64encode(image_file.read()).decode("utf-8")
+        msg = fetch_chain(inputs={"image": f"data:image/png;base64,{base64_string}"}, type='props')
+        ans_payload = json.loads(msg)
+        ans_payload.update({"id": ind, "uri": img_uri})
+        print("Props->", ans_payload)
+        IMG_DESC.append(ans_payload)
+
+    prop_vectorstore = Chroma(
+        collection_name="whiteboard_props_v1",
+        persist_directory='./chroma',
+        embedding_function=OpenAIEmbeddings()
+    )
+
+    prop_vectorstore.add_texts(texts=[obj['visual_description'] for obj in IMG_DESC], metadatas=IMG_DESC)
+    print("Props embeddings saved successfully")
+
+embed_scenes()
+embed_props_and_characters()
--- image_search_v3.py
+++ image_search_v3.py
@@ -6,14 +6,41 @@ from langchain_experimental.open_clip import OpenCLIPEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
 
 
-query = "female doctor"
-txt_vectorstore = Chroma(
-    collection_name="hospital_img_openai",#"pokemon_text_v4",
-    persist_directory='./chroma',
-    embedding_function=OpenAIEmbeddings()
-)
+def scene_search(query):
+    scene_vectorstore = Chroma(
+        collection_name="whiteboard_scenes_v1",
+        persist_directory='./chroma',
+        embedding_function=OpenAIEmbeddings()
+    )
+    character_vectorstore = Chroma(
+        collection_name="whiteboard_characters_v1",
+        persist_directory='./chroma',
+        embedding_function=OpenAIEmbeddings()
+    )
+    prop_vectorstore = Chroma(
+        collection_name="whiteboard_props_v1",
+        persist_directory='./chroma',
+        embedding_function=OpenAIEmbeddings()
+    )
 
-txt_results = []
-docs = txt_vectorstore.similarity_search_with_relevance_scores(query, k=1)
-for ind, doc in enumerate(docs):
-    print(doc[0].metadata['uri'], doc[0].metadata['id'], doc[0].page_content, doc[1])
+    scene_docs = scene_vectorstore.similarity_search_with_relevance_scores(query, k=1)
+    for ind, scene_doc in enumerate(scene_docs):
+        print(f"Scene Image: {scene_doc[0].metadata['uri']}")
+        print(f"Animated motions in the scene: {scene_doc[0].metadata['motions']}")
+        print("Similar Characters:")
+        for ind, character in enumerate(scene_doc[0].metadata['characters'].split(",")):
+            print(f"    Character {ind+1}: ", character)
+            character_docs = character_vectorstore.similarity_search_with_relevance_scores(character, k=5)
+            for ind, character_doc in enumerate(character_docs):
+                print(f"        - {character_doc[0].metadata['uri']} {character_doc[0].metadata['visual_description']} {character_doc[1]}")
+        print("Similar Props:")
+        for ind, prop in enumerate(scene_doc[0].metadata['props'].split(",")):
+            print(f"    Prop {ind+1}: ", prop)
+            prop_docs = prop_vectorstore.similarity_search_with_relevance_scores(prop, k=5)
+            for ind, prop_doc in enumerate(prop_docs):
+                print(f"        - {prop_doc[0].metadata['uri']} {prop_doc[0].metadata['visual_description']} {prop_doc[1]}")
+
+query = '''
+A calm hospice setting. A hospice care worker is gently washing their hands.
+'''
+scene_search(query=query)
